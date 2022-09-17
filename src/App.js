79689edc2ba7ffda31b9131022a498ab273fbb6a
Bolu Tejumola <tejumolabolu@gmail.com>
2022-09-17 20:14:19 +0100

    Copyright

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         setQuotes(data[randomNum]);
       });
   };
+
   useEffect(() => {
     getQuote();
   }, []);
@@ -25,14 +26,14 @@ function App() {
   return (
     <div className="min-h-screen flex bg-indigo-700">
       <div className="m-auto w-full max-w-md min-h-min px-8 py-8 bg-gray-700 shadow-slate-900 flex flex-col justify-between items-start">
-        <p ref={textRef} className="text-white text-2xl break-normal">{quotes.text}</p>
+        <p ref={textRef} className="text-white text-2xl break-normal">{quotes.text} </p>
         <p className="my-2.5 text-white text-2xl ">Author: {quotes.author}</p>
         <div>
           <button onClick={getQuote} className="mr-3 bg-sky-600 text-white w-20 py-2 text-sm rounded-md cursor-pointer hover:bg-sky-800  ">
             Get quotes
           </button>
           <a
-            href={`https://twitter.com/intent/tweet?text=${quotes.text}`}
+            href={`https://twitter.com/intent/tweet?text=${quotes.text}  %0a %0a Shared from: %0a https://random-quo.netlify.app`}
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-center bg-sky-600 text-white w-20 py-2 text-sm rounded-md cursor-pointer hover:bg-sky-800 "
